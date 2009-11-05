d174c1cfdef713dadf6b8e009f8c0b04eb8ec97b
Jeff King <peff@peff.net>
2009-11-05 07:08:55 -0500

    add 'clock' script
    This wraps the timecard program to provide a
    pseudo-"distributed" timecard. We use a git repository to
    push and pull changes from a central location, which should
    notice any problems with multiple simultaneous updates
    (though it will do nothing but just barf and let you fix up
    the result yourself, so you do need to be at least a little
    bit git-competent).

---
+++ clock.sh
@@ -0,0 +1,22 @@
+set -e
+
+cd $HOME/.timecard
+
+mutator() {
+  case "$1" in
+    in|out|comment|edit) return 0 ;;
+    *) return 1 ;;
+  esac
+}
+
+if mutator "$@"; then
+  git pull -q
+fi
+
+TIMECARD=timecard timecard "$@"
+
+if mutator "$@"; then
+  git add -A
+  git commit -q -m "clock $*"
+  git push -q
+fi
